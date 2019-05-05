dd50c393142a217ac47aa868f0c0fba9ac260b84
Saket Vats <saketvatslpu@gmail.com>
2019-05-05 12:03:31 +0530

    Testing the .net core with VS Code

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace C_Sharp_GetAll_Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Saket\Desktop\C_Sharp_GetAll_Files";
+            DirectoryInfo d = new DirectoryInfo(path);
+            foreach (var i in d.GetFiles()) { Console.WriteLine(i.FullName + "\n"); }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
